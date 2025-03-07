085413a429a27b25e1d06c1e0dd92e38ed897266
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2025-03-07 11:24:55 -0600

    Attempt to add tailwind prettier plugin

--- src/App.svelte
+++ src/App.svelte
@@ -70,6 +70,12 @@
       />
       <button type="submit" disabled={isLoading}>Send</button>
     </form>
+
+    <button
+      class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800"
+    >
+      Submit
+    </button>
   </div>
 </main>
 
